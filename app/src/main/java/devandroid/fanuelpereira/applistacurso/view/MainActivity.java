e0a5bdb1e6f33aa750dd098abd8aabad07435f70
fanuelhpa <fanuelrique@gmail.com>
2023-04-20 18:20:44 -0300

    Associação dos componentes de tela ao código.

--- app/src/main/java/devandroid/fanuelpereira/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/fanuelpereira/applistacurso/view/MainActivity.java
@@ -2,6 +2,9 @@ package devandroid.fanuelpereira.applistacurso.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import devandroid.fanuelpereira.applistacurso.R;
 import devandroid.fanuelpereira.applistacurso.model.Pessoa;
@@ -9,6 +12,14 @@ import devandroid.fanuelpereira.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
+    EditText edtTxtPrimeiroNome;
+    EditText edtTxtSobrenome;
+    EditText edtTxtCursoDesejado;
+    EditText edtTxtTelefoneContato;
+
+    Button btnLimpar;
+    Button btnSalvar;
+    Button btnFinalizar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +33,19 @@ public class MainActivity extends AppCompatActivity {
     pessoa.setCursoDesejado("Android");
     pessoa.setTelefoneContato("(31) 99248-6568");
 
+    edtTxtPrimeiroNome = findViewById(R.id.edtTxtPrimeiroNome);
+    edtTxtSobrenome = findViewById(R.id.edtTxtSobrenome);
+    edtTxtCursoDesejado = findViewById(R.id.edtTxtCursoDesejado);
+    edtTxtTelefoneContato = findViewById(R.id.edtTxtTelefoneContato);
+    btnLimpar = findViewById(R.id.btnLimpar);
+    btnSalvar = findViewById(R.id.btnSalvar);
+    btnFinalizar = findViewById(R.id.btnFinalizar);
+
+    edtTxtPrimeiroNome.setText(pessoa.getPrimeiroNome());
+    edtTxtSobrenome.setText(pessoa.getSobreNome());
+    edtTxtTelefoneContato.setText(pessoa.getTelefoneContato());
+    edtTxtCursoDesejado.setText(pessoa.getCursoDesejado());
+
+    int mark = 0;
     }
 }
